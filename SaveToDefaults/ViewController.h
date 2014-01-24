0cae229cb4998dc82fba1d3670fee22db4fe2dfc
fsuskate <francis.surroca@gmail.com>
2014-01-23 22:04:08 -0500

    Initial Commit

---
+++ SaveToDefaults/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SaveToDefaults
+//
+//  Created by Francisco Surroca on 1/23/14.
+//  Copyright (c) 2014 Francisco Surroca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SaveToDefaults/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SaveToDefaults
+//
+//  Created by Francisco Surroca on 1/23/14.
+//  Copyright (c) 2014 Francisco Surroca. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
