4ad70ff0384ce7d8728c641c5795da2056b97f8f
fsuskate <francis.surroca@gmail.com>
2014-01-24 23:53:57 -0500

    Update

--- SaveToDefaults/ViewController.h
+++ SaveToDefaults/ViewController.h
@@ -9,5 +9,16 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    IBOutlet UIButton *loginButton;
+    IBOutlet UITextField *userNameTextField;
+    IBOutlet UILabel *userNameLabel;
+    
+    IBOutlet UITextField *passwordTextField;
+    IBOutlet UILabel *passwordLabel;
+}
+
+-(IBAction)login;
+
 
 @end
--- SaveToDefaults/ViewController.m
+++ SaveToDefaults/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "SecondViewController.h"
 
 @interface ViewController ()
 
@@ -14,10 +15,62 @@
 
 @implementation ViewController
 
+-(BOOL)checkUserName
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *userNameString = [defaults objectForKey:@"userName"];
+    NSString *passwordString = [defaults objectForKey:@"password"];
+    
+    if ([userNameString isEqualToString:@"TimTest"] && [passwordString isEqualToString:@"tester1"])
+    {
+        return true;
+    }
+    else if ([userNameString isEqualToString:@"BethTest"] && [passwordString isEqualToString:@"tester2"])
+    {
+        return true;
+    }
+    return false;
+}
+
+-(IBAction)login
+{
+    NSString *userNameString = userNameTextField.text;
+    NSString *passwordString = passwordTextField.text;
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:userNameString forKey:@"userName"];
+    [defaults setObject:passwordString forKey:@"password"];
+
+    [defaults synchronize];
+    
+    if ([self checkUserName] == false)
+    {
+        NSString *message = [NSString stringWithFormat:@"Username or password is incorrect: \n%@\n%@", userNameString, passwordString];
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Access Denied"
+                                                        message:message
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alertView show];
+        return;
+    }
+    
+    SecondViewController *view = [self.storyboard instantiateViewControllerWithIdentifier:@"logout"];
+    if (view != nil)
+    {
+        [self presentModalViewController:view animated:YES];
+    }
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *userNameString = [defaults objectForKey:@"userName"];
+    NSString *passwordString = [defaults objectForKey:@"password"];
+    [userNameTextField setText:userNameString];
+    [passwordTextField setText:passwordString];
 }
 
 - (void)didReceiveMemoryWarning
